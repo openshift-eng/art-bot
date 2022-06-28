a77a37a8d97eab337cb6bf43acfcff79a5a546c9
Ashwin Das <ashwindasr@gmail.com>
2022-06-28 01:22:25 -0400

    add tests, comments

--- artbotlib/pipeline_image_util.py
+++ artbotlib/pipeline_image_util.py
@@ -7,17 +7,23 @@ from artbotlib import exceptions
 
 
 # Functions for pipeline from GitHub
-def github_repo_is_available(repo_name):
+def github_repo_is_available(repo_name: str) -> bool:
     """
     Function to check whether the given GitHub repo name is valid
 
-    :repo_nae: The name of the GitHub repo.
+    :repo_name: The name of the GitHub repo.
     """
     response = requests.head(f"https://github.com/openshift/{repo_name}")
     return response.status_code == 200
 
 
-def github_to_distgit(github_name, version):
+def github_to_distgit(github_name: str, version: str) -> list:
+    """
+    Driver function to get the GitHub to distgit mappings from the GitHub name and OCP version.
+
+    :github_name: The name of the GitHub repo
+    :version: OCP version
+    """
     data = github_distgit_mappings(version)
     distgits = data[github_name]
     if not distgits:
@@ -27,7 +33,7 @@ def github_to_distgit(github_name, version):
 
 
 # Distgit
-def distgit_is_available(distgit_repo_name):
+def distgit_is_available(distgit_repo_name: str) -> bool:
     """
     Function to check whether the given distgit repo name is valid
 
@@ -37,7 +43,13 @@ def distgit_is_available(distgit_repo_name):
     return response.status_code == 200
 
 
-def distgit_to_github(distgit_name, version):
+def distgit_to_github(distgit_name: str, version: str) -> str:
+    """
+    Driver function to get the distgit to GitHub mappings from the GitHub name and OCP version.
+
+    :distgit_name: The name of the distgit repo
+    :version: OCP version
+    """
     data = distgit_github_mappings(version)
     try:
         return data[distgit_name].split('/')[-1]
@@ -46,7 +58,7 @@ def distgit_to_github(distgit_name, version):
             f"Couldn't find GitHub repo from distgit `{distgit_name}` and version `{version}`")
 
 
-def distgit_to_brew(distgit_name, version):
+def distgit_to_brew(distgit_name: str, version: str) -> str:
     """
     Get the brew name from the distgit name.
 
@@ -69,7 +81,14 @@ def distgit_to_brew(distgit_name, version):
         return brew_name
 
 
-def distgit_to_delivery(distgit_repo_name, version, variant):
+def distgit_to_delivery(distgit_repo_name: str, version: str, variant: str) -> str:
+    """
+    Driver function for distgit -> delivery pipeline.
+
+    :distgit_repo_name: The name of the distgit repo
+    :version: OCP version
+    :variant: RHOSE variant
+    """
     payload = ""
 
     # Tag
@@ -94,12 +113,11 @@ def distgit_to_delivery(distgit_repo_name, version, variant):
 
     # Brew -> Delivery
     payload += brew_to_delivery(brew_package_name, variant)
-
     return payload
 
 
 # Brew stuff
-def brew_is_available(brew_name):
+def brew_is_available(brew_name: str) -> bool:
     """
     Function to check whether the given brew repo is valid
 
@@ -112,7 +130,13 @@ def brew_is_available(brew_name):
         return False
 
 
-def brew_to_github(brew_name, version):
+def brew_to_github(brew_name: str, version: str) -> str:
+    """
+    Driver function for Brew -> GitHub pipeline
+
+    :brew_name: Name of the brew package
+    :version: OCP version
+    """
     payload = ""
 
     # Distgit
@@ -144,7 +168,7 @@ def brew_to_github(brew_name, version):
     return payload
 
 
-def get_brew_id(brew_name):
+def get_brew_id(brew_name: str) -> int:
     """
     Get the brew id for the given brew name.
 
@@ -164,7 +188,7 @@ def get_brew_id(brew_name):
     return brew_id
 
 
-def brew_to_cdn(brew_name, variant_name):
+def brew_to_cdn(brew_name: str, variant_name: str) -> list:
     """
     Function to return all the Brew to CDN mappings (since more than one could be present)
 
@@ -194,7 +218,13 @@ def brew_to_cdn(brew_name, variant_name):
     return results
 
 
-def brew_to_delivery(brew_package_name, variant):
+def brew_to_delivery(brew_package_name: str, variant: str) -> str:
+    """
+    Driver function for Brew -> Delivery pipeline
+
+    :brew_package_name: Brew package name
+    :variant: The 8Base-RHOSE variant
+    """
     payload = ""
     cdn_repo_names = brew_to_cdn(brew_package_name, variant)
     if len(cdn_repo_names) > 1:
@@ -206,12 +236,11 @@ def brew_to_delivery(brew_package_name, variant):
 
         # CDN -> Delivery
         payload += cdn_to_delivery_payload(cdn_repo_name)
-
     return payload
 
 
 @util.cached
-def doozer_brew_distgit(version):
+def doozer_brew_distgit(version: str) -> list:
     output = util.cmd_gather(f"doozer -g openshift-{version} images:print --short '{{component}}: {{name}}'")
     if "koji.GSSAPIAuthError" in output[2]:
         raise exceptions.KerberosAuthenticationError("Kerberos authentication failed for doozer")
@@ -224,7 +253,13 @@ def doozer_brew_distgit(version):
     return result
 
 
-def brew_to_distgit(brew_name, version):
+def brew_to_distgit(brew_name: str, version: str) -> str:
+    """
+    Function to get the distgit name from the brew package name
+
+    :brew_name: The name of the brew package
+    :version: OCP version. Eg: 4.10
+    """
     output = doozer_brew_distgit(version)
 
     dict_data = {}
@@ -233,7 +268,7 @@ def brew_to_distgit(brew_name, version):
             dict_data[line[0]] = line[1]
 
     if not dict_data:
-        raise exceptions.NullDataReturned("No data from doozer command for brew-distigit mapping")
+        raise exceptions.NullDataReturned("No data from doozer command for brew-distgit mapping")
     try:
         return dict_data[brew_name]
     except Exception:
@@ -241,7 +276,12 @@ def brew_to_distgit(brew_name, version):
 
 
 # CDN stuff
-def cdn_is_available(cdn_name):
+def cdn_is_available(cdn_name: str) -> bool:
+    """
+    Function to check if the given CDN repo name is available
+
+    :cdn_name: Name of the CDN repo
+    """
     try:
         _ = get_cdn_repo_details(cdn_name)
         return True
@@ -249,7 +289,12 @@ def cdn_is_available(cdn_name):
         return False
 
 
-def get_cdn_repo_details(cdn_name):
+def get_cdn_repo_details(cdn_name: str) -> dict:
+    """
+    Function to get the details regarding the given CDN repo.
+
+    :cdn_name: The name of the CDN repo
+    """
     url = f"https://errata.devel.redhat.com/api/v1/cdn_repos/{cdn_name}"
     response = request_with_kerberos(url)
 
@@ -259,7 +304,7 @@ def get_cdn_repo_details(cdn_name):
     return response.json()
 
 
-def cdn_to_delivery(cdn_name):
+def cdn_to_delivery(cdn_name: str) -> str:
     """
     Function to get the delivery repo name from the CDN repo name.
 
@@ -273,7 +318,7 @@ def cdn_to_delivery(cdn_name):
         raise exceptions.DeliveryRepoNotFound(f"Delivery Repo not found for CDN `{cdn_name}`")
 
 
-def get_cdn_repo_id(cdn_name):
+def get_cdn_repo_id(cdn_name: str) -> int:
     """
     Function to get the CDN repo ID. Used to construct the CDN repo URL to direct to its page in Errata.
 
@@ -287,7 +332,12 @@ def get_cdn_repo_id(cdn_name):
         raise exceptions.CdnIdNotFound(f"CDN ID not found for CDN `{cdn_name}`")
 
 
-def cdn_to_brew(cdn_name):
+def cdn_to_brew(cdn_name: str) -> str:
+    """
+    Function to get the brew name from the given CDN name
+
+    :cdn_name: The CDN repo name
+    """
     response = get_cdn_repo_details(cdn_name)
 
     brew_packages = response['data']['relationships']['packages']
@@ -299,7 +349,7 @@ def cdn_to_brew(cdn_name):
         raise exceptions.BrewNotFoundFromCdnApi("Brew package not mapped to CDN in Errata. Contact ART.")
 
 
-def get_variant_id(cdn_name, variant_name):
+def get_variant_id(cdn_name: str, variant_name: str) -> int:
     """
     Function to get the id of the product variant. Used to get the product ID.
 
@@ -316,7 +366,7 @@ def get_variant_id(cdn_name, variant_name):
         raise exceptions.VariantIdNotFound(f"Variant ID not found for CDN `{cdn_name}` and variant `{variant_name}`")
 
 
-def get_product_id(variant_id):
+def get_product_id(variant_id: int) -> int:
     """
     Function to get the product id. Used to construct the CDN repo URL to direct to its page in Errata.
 
@@ -331,7 +381,13 @@ def get_product_id(variant_id):
         raise exceptions.ProductIdNotFound(f"Product ID not found for variant `{variant_id}`")
 
 
-def cdn_to_github(cdn_name, version):
+def cdn_to_github(cdn_name: str, version: str) -> str:
+    """
+    Driver function for the CDN -> GitHub pipeline
+
+    :cdn_name: The name of the CDN repo
+    :version: The OCP version
+    """
     payload = ""
 
     # Brew
@@ -342,25 +398,41 @@ def cdn_to_github(cdn_name, version):
     # Brew -> GitHub
     payload += brew_to_github(brew_name, version)
     # Use after brew to distgit mapping fixed
-
     return payload
 
 
-def get_cdn_payload(cdn_repo_name, variant):
+def get_cdn_payload(cdn_repo_name: str, variant: str) -> str:
+    """
+    Function to get the CDN payload for slack (since it's needed in multiple places.
+
+    :cdn_repo_name: The name of the CDN repo
+    :variant: The 8Base-RHOSE variant
+    """
     cdn_repo_id = get_cdn_repo_id(cdn_repo_name)
     variant_id = get_variant_id(cdn_repo_name, variant)
     product_id = get_product_id(variant_id)
+
     return f"CDN repo: <https://errata.devel.redhat.com/product_versions/{product_id}/cdn_repos/{cdn_repo_id}|*{cdn_repo_name}*>\n"
 
 
-def cdn_to_delivery_payload(cdn_repo_name):
+def cdn_to_delivery_payload(cdn_repo_name: str):
+    """
+    Function to get the CDN payload for slack (since it's needed in multiple places.
+
+    :cdn_repo_name: The name of the CDN repo
+    """
     delivery_repo_name = cdn_to_delivery(cdn_repo_name)
     delivery_repo_id = get_delivery_repo_id(delivery_repo_name)
     return f"Delivery (Comet) repo: <https://comet.engineering.redhat.com/containers/repositories/{delivery_repo_id}|*{delivery_repo_name}*>\n\n"
 
 
 # Delivery stuff
-def delivery_repo_is_available(name):
+def delivery_repo_is_available(name: str) -> bool:
+    """
+    Function to check if the given delivery repo exists
+
+    :name: Name of the delivery repo
+    """
     try:
         _ = get_delivery_repo_id(name)
         return True
@@ -368,7 +440,10 @@ def delivery_repo_is_available(name):
         return False
 
 
-def brew_from_delivery(delivery_repo):
+def brew_from_delivery(delivery_repo: str) -> str:
+    """
+    Function to get the brew name from the delivery repo
+    """
     url = f"https://pyxis.engineering.redhat.com/v1/repositories/registry/registry.access.redhat.com/repository/{delivery_repo}/images"
     response = request_with_kerberos(url)
 
@@ -387,7 +462,14 @@ def brew_from_delivery(delivery_repo):
     return result.pop()
 
 
-def brew_to_cdn_delivery(brew_name, variant, delivery_repo_name):
+def brew_to_cdn_delivery(brew_name: str, variant: str, delivery_repo_name: str) -> str:
+    """
+    Function the get the CDN name from brew name, variant and delivery repo name
+
+    :brew_name: Brew repo name
+    :variant: 8Base-RHOSE variant
+    :delivery_repo_name: Delivery repo name
+    """
     cdn_repo_names = brew_to_cdn(brew_name, variant)
     for cdn_repo_name in cdn_repo_names:
         delivery = cdn_to_delivery(cdn_repo_name)
@@ -397,7 +479,7 @@ def brew_to_cdn_delivery(brew_name, variant, delivery_repo_name):
         f"Could not find CDN from Brew name from delivery repo `{delivery_repo_name}`")
 
 
-def get_delivery_repo_id(name):
+def get_delivery_repo_id(name: str) -> str:
     """
     Function to get the delivery repo id. Used to construct the delivery repo URL to direct to its page in Pyxis.
 
@@ -418,7 +500,7 @@ def get_delivery_repo_id(name):
 
 
 # Methods
-def request_with_kerberos(url):
+def request_with_kerberos(url: str) -> requests.Response():
     # Kerberos authentication
     kerberos_auth = HTTPKerberosAuth(mutual_authentication=OPTIONAL)
 
@@ -426,12 +508,12 @@ def request_with_kerberos(url):
     response = requests.get(url, auth=kerberos_auth)
 
     if response.status_code == 401:
-        raise exceptions.KerberosAuthenticationError("")
+        raise exceptions.KerberosAuthenticationError("Kerberos Authentication failed")
 
     return response
 
 
-def get_image_stream_tag(distgit_name, version):
+def get_image_stream_tag(distgit_name: str, version: str) -> str:
     """
     Function to get the image stream tag if the image is a payload image.
     The for_payload flag would be set to True in the yml file
@@ -449,7 +531,12 @@ def get_image_stream_tag(distgit_name, version):
 
 
 @util.cached
-def doozer_github_distgit(version):
+def doozer_github_distgit(version: str) -> list:
+    """
+    Function to get the distgit to GitHub mappings from doozer
+
+    :version: OCP version
+    """
     output = util.cmd_gather(f"doozer -g openshift-{version} images:print --short '{{name}}: {{upstream_public}}'")
     if "koji.GSSAPIAuthError" in output[2]:
         raise exceptions.KerberosAuthenticationError("Kerberos authentication failed for doozer")
@@ -462,7 +549,7 @@ def doozer_github_distgit(version):
     return result
 
 
-def github_distgit_mappings(version):
+def github_distgit_mappings(version: str) -> dict:
     """
     Function to get the GitHub to Distgit mappings present in a particular OCP version.
 
@@ -479,7 +566,7 @@ def github_distgit_mappings(version):
     return dict_data
 
 
-def distgit_github_mappings(version):
+def distgit_github_mappings(version: str) -> dict:
     """
     Function to get the distgit to GitHub mappings present in a particular OCP version.
 
@@ -491,11 +578,17 @@ def distgit_github_mappings(version):
         if len(line) == 2:
             dict_data[line[0]] = line[1]
     if not dict_data:
-        raise exceptions.NullDataReturned("No data from doozer command for distgit-distgit mapping")
+        raise exceptions.NullDataReturned("No data from doozer command for distgit-github mapping")
     return dict_data
 
 
-def require_bundle_build(distgit_name, version):
+def require_bundle_build(distgit_name: str, version: str) -> bool:
+    """
+    Function to check if bundle build details need to be displayed
+
+    :distgit_name: Name of the distgit repo
+    :version: OCP version
+    """
     url = f"https://raw.githubusercontent.com/openshift/ocp-build-data/openshift-{version}/images/{distgit_name}.yml"
     response = requests.get(url)
 
@@ -511,7 +604,13 @@ def require_bundle_build(distgit_name, version):
         return False
 
 
-def get_bundle_override(distgit_name, version):
+def get_bundle_override(distgit_name: str, version: str) -> str | None:
+    """
+    Check the yml file for an override for the bundle component name. Else return None
+
+    :distgit_name: The name of the distgit repo
+    :version: The OCP version
+    """
     url = f"https://raw.githubusercontent.com/openshift/ocp-build-data/openshift-{version}/images/{distgit_name}.yml"
     response = requests.get(url)
 
--- functional/test_pipeline_image_names.py
+++ functional/test_pipeline_image_names.py
@@ -1,18 +1,74 @@
 import pytest
-from artbotlib.pipeline_image_names import distgit_to_brew, brew_to_cdn, cdn_to_delivery, distgit_is_available, \
-    get_image_stream_tag, get_delivery_repo_id, get_brew_id
 from artbotlib import exceptions
+from artbotlib import pipeline_image_util as img_util
+
+
+def test_availability_github1():
+    test_repo = "coredns"
+    response = img_util.github_repo_is_available(test_repo)
+
+    assert response
+
+
+def test_availability_github2():
+    test_repo = "coredns1234"
+    response = img_util.github_repo_is_available(test_repo)
+
+    assert not response
+
+
+def test_github_to_distgit1():
+    test_repo = "cluster-resource-override-admission-operator"
+    response = img_util.github_to_distgit(test_repo, "4.10")
+
+    assert response is not None
+
+
+def test_github_to_distgit2():
+    test_repo = "cluster-resource-override-admission-operator1234"
+
+    with pytest.raises(Exception) as e:
+        _ = img_util.github_to_distgit(test_repo, "4.10")
+    assert e.type == exceptions.DistgitFromGithubNotFound
+
+
+def test_distgit_repo_availability1():
+    response = img_util.distgit_is_available("openshift-enterprise-cli")
+
+    assert response
+
+
+def test_distgit_repo_availability2():
+    response = img_util.distgit_is_available("booyah")
+
+    assert response is not None
+
+
+def test_distgit_to_github1():
+    distgit = "clusterresourceoverride-operator"
+    actual = img_util.distgit_to_github(distgit, "4.10")
+    expected = "cluster-resource-override-admission-operator"
+
+    assert actual == expected
+
+
+def test_distgit_to_github2():
+    distgit = "clusterresourceoverride-operator1234"
+
+    with pytest.raises(Exception) as e:
+        _ = img_util.distgit_to_github(distgit, "4.10")
+    assert e.type == exceptions.GithubFromDistgitNotFound
 
 
 def test_distgit_to_brew_1():
-    actual = distgit_to_brew("openshift-enterprise-console", version="4.11")
+    actual = img_util.distgit_to_brew("openshift-enterprise-console", version="4.11")
     expected = "openshift-enterprise-console-container"
 
     assert actual == expected
 
 
 def test_distgit_to_brew_2():
-    actual = distgit_to_brew("egress-router-cni", version="4.11")
+    actual = img_util.distgit_to_brew("egress-router-cni", version="4.11")
     expected = "ose-egress-router-cni-container"
 
     assert actual == expected
@@ -20,52 +76,48 @@ def test_distgit_to_brew_2():
 
 def test_distgit_to_brew_3():
     with pytest.raises(Exception) as e:
-        distgit_to_brew("openshift-enterprise-console", version="4.00")
+        img_util.distgit_to_brew("openshift-enterprise-console", version="4.00")
     assert e.type == exceptions.DistgitNotFound
 
 
-def test_brew_to_cdn1():
-    actual = brew_to_cdn("openshift-enterprise-console-container", "8Base-RHOSE-4.10")
-    expected = ["redhat-openshift4-ose-console"]
+def test_distgit_delivery():
+    actual = img_util.distgit_to_delivery("clusterresourceoverride-operator", "4.10", "8Base-RHOSE-4.10")
+    expected = """Production brew builds: <https://brewweb.engineering.redhat.com/brew/packageinfo?packageID=73711|*ose-clusterresourceoverride-operator-container*>
+Bundle Component: *ose-clusterresourceoverride-operator-metadata-component*
+Bundle Distgit: *clusterresourceoverride-operator-bundle*
+CDN repo: <https://errata.devel.redhat.com/product_versions/1625/cdn_repos/12950|*redhat-openshift4-ose-clusterresourceoverride-rhel8-operator*>
+Delivery (Comet) repo: <https://comet.engineering.redhat.com/containers/repositories/5f6d2a2049dbe0cdd0373f29|*openshift4/ose-clusterresourceoverride-rhel8-operator*>\n\n"""
 
     assert actual == expected
 
 
-def test_brew_to_cdn2():
-    with pytest.raises(Exception) as e:
-        brew_to_cdn("openshift-enterprise-console-container", "8Base-RHOSED-4.10")
-    assert e.type == exceptions.CdnFromBrewNotFound
+def test_brew_is_available1():
+    test_repo = "ose-clusterresourceoverride-operator-container"
+    response = img_util.brew_is_available(test_repo)
+
+    assert response
 
 
-def test_cdn_to_comet1():
-    actual = cdn_to_delivery("redhat-openshift4-ose-console")
-    expected = "openshift4/ose-console"
+def test_brew_is_available2():
+    test_repo = "ose-clusterresourceoverride-operator-container1234"
+    response = img_util.brew_is_available(test_repo)
 
-    assert actual == expected
-
-
-def test_cdn_to_comet2():
-    with pytest.raises(Exception) as e:
-        cdn_to_delivery("redhat-openshift4-ose-consoleeee")
-    assert e.type == exceptions.DeliveryRepoNotFound or e.type == exceptions.CdnNotFound
+    assert not response
 
 
-def test_distgit_repo_availability1():
-    actual = distgit_is_available("openshift-enterprise-cli")
-    expected = True
-
-    assert actual == expected
-
-
-def test_distgit_repo_availability2():
-    actual = distgit_is_available("booyah")
-    expected = False
+def test_brew_to_github():
+    actual = img_util.brew_to_github("ose-clusterresourceoverride-operator-container", "4.10")
+    expected = """Upstream GitHub repository: <https://github.com/openshift/cluster-resource-override-admission-operator|*openshift/cluster-resource-override-admission-operator*>
+Private GitHub repository: <https://github.com/openshift-priv/cluster-resource-override-admission-operator|*openshift-priv/cluster-resource-override-admission-operator*>
+Production dist-git repo: <https://pkgs.devel.redhat.com/cgit/containers/clusterresourceoverride-operator|*clusterresourceoverride-operator*>
+Bundle Component: *ose-clusterresourceoverride-operator-metadata-component*
+Bundle Distgit: *clusterresourceoverride-operator-bundle*\n"""
 
     assert actual == expected
 
 
 def test_get_brew_id1():
-    actual = get_brew_id("openshift-enterprise-console-container")
+    actual = img_util.get_brew_id("openshift-enterprise-console-container")
     expected = 69142
 
     assert actual == expected
@@ -73,26 +125,192 @@ def test_get_brew_id1():
 
 def test_get_brew_id2():
     with pytest.raises(Exception) as e:
-        get_brew_id("openshift-enterprise-console-container-booyah")
+        _ = img_util.get_brew_id("openshift-enterprise-console-container-booyah")
     assert e.type == exceptions.BrewIdNotFound
 
 
-def test_get_delivery_repo_id():
-    actual = get_delivery_repo_id("openshift4/ose-cli")
-    expected = "5cd9ba3f5a13467289f4d51d"
+def test_brew_to_cdn1():
+    actual = img_util.brew_to_cdn("openshift-enterprise-console-container", "8Base-RHOSE-4.10")
+    expected = ["redhat-openshift4-ose-console"]
 
     assert actual == expected
 
 
-def test_check_for_payload1():
-    actual = get_image_stream_tag("ose-metallb", "4.10")
+def test_brew_to_cdn2():
+    with pytest.raises(Exception) as e:
+        _ = img_util.brew_to_cdn("openshift-enterprise-console-container123", "8Base-RHOSE-4.10")
+    assert e.type == exceptions.CdnFromBrewNotFound
+
+
+def test_brew_to_delivery():
+    actual = img_util.brew_to_delivery("ose-clusterresourceoverride-operator-container", "8Base-RHOSE-4.10")
+    expected = """CDN repo: <https://errata.devel.redhat.com/product_versions/1625/cdn_repos/12950|*redhat-openshift4-ose-clusterresourceoverride-rhel8-operator*>
+Delivery (Comet) repo: <https://comet.engineering.redhat.com/containers/repositories/5f6d2a2049dbe0cdd0373f29|*openshift4/ose-clusterresourceoverride-rhel8-operator*>\n\n"""
+
+    assert actual == expected
+
+
+def test_doozer_brew_distgit():
+    response = img_util.doozer_brew_distgit("4.10")
+
+    assert len(response) == 208
+
+
+def test_brew_to_distgit():
+    actual = img_util.brew_to_distgit("ose-clusterresourceoverride-operator-container", "4.10")
+    expected = "clusterresourceoverride-operator"
+
+    assert actual == expected
+
+
+def test_cdn_is_available1():
+    response = img_util.cdn_is_available("redhat-openshift4-ose-clusterresourceoverride-rhel8-operator")
+
+    assert response
+
+
+def test_cdn_is_available2():
+    response = img_util.cdn_is_available("not-a-cdn-repo1234")
+
+    assert not response
+
+
+def test_get_cdn_repo_details1():
+    response = img_util.get_cdn_repo_details("redhat-openshift4-ose-clusterresourceoverride-rhel8-operator")
+    assert response is not None
+
+
+def test_get_cdn_repo_details2():
+    with pytest.raises(Exception) as e:
+        _ = img_util.get_cdn_repo_details("not-a-cdn-repo1234")
+    assert e.type == exceptions.CdnNotFound
+
+
+def cdn_to_delivery1():
+    actual = img_util.cdn_to_delivery("redhat-openshift4-ose-console")
+    expected = "openshift4/ose-console"
+
+    assert actual == expected
+
+
+def cdn_to_delivery2():
+    with pytest.raises(Exception) as e:
+        _ = img_util.cdn_to_delivery("redhat-openshift4-ose-consoleeee")
+    assert e.type == exceptions.DeliveryRepoNotFound or e.type == exceptions.CdnNotFound
+
+
+def test_get_cdn_repo_id():
+    actual = img_util.get_cdn_repo_id("redhat-openshift4-ose-clusterresourceoverride-rhel8-operator")
+    expected = 12950
+
+    assert actual == expected
+
+
+def test_cdn_to_brew():
+    actual = img_util.cdn_to_brew("redhat-openshift4-ose-clusterresourceoverride-rhel8-operator")
+    expected = "ose-clusterresourceoverride-operator-container"
+
+    assert actual == expected
+
+
+def test_get_variant_id():
+    actual = img_util.get_variant_id("redhat-openshift4-ose-cli-alt-rhel8", "8Base-RHOSE-4.10")
+    expected = 3678
+
+    assert actual == expected
+
+
+def test_get_product_id():
+    actual = img_util.get_product_id(3678)
+    expected = 1625
+
+    assert actual == expected
+
+
+def test_cdn_to_github():
+    actual = img_util.cdn_to_github("redhat-openshift4-ose-cli-alt-rhel8", "4.10")
+    expected = """Production brew builds: <https://brewweb.engineering.redhat.com/brew/packageinfo?packageID=79953|*openshift-enterprise-cli-alt-container*>
+Upstream GitHub repository: <https://github.com/openshift/oc|*openshift/oc*>
+Private GitHub repository: <https://github.com/openshift-priv/oc|*openshift-priv/oc*>
+Production dist-git repo: <https://pkgs.devel.redhat.com/cgit/containers/openshift-enterprise-cli-alt|*openshift-enterprise-cli-alt*>
+Payload tag: *cli-alt* 
+"""
+    assert actual == expected
+
+
+def test_delivery_repo_is_available1():
+    response = img_util.delivery_repo_is_available("openshift4/ose-cli-alt-rhel8")
+
+    assert response
+
+
+def test_delivery_repo_is_available2():
+    response = img_util.delivery_repo_is_available("openshift4/not-a-cdn-repo-1321")
+
+    assert not response
+
+
+def test_brew_from_delivery():
+    actual = img_util.brew_from_delivery("openshift4/ose-cli-alt-rhel8")
+    expected = "openshift-enterprise-cli-alt-container"
+
+    assert actual == expected
+
+
+def test_brew_to_cdn_delivery1():
+    actual = img_util.brew_to_cdn_delivery("openshift-enterprise-cli-alt-container", "8Base-RHOSE-4.10",
+                                           "openshift4/ose-cli-alt-rhel8")
+    expected = "redhat-openshift4-ose-cli-alt-rhel8"
+
+    assert actual == expected
+
+
+def test_get_delivery_repo_id1():
+    actual = img_util.get_delivery_repo_id("openshift4/ose-cli-alt-rhel8")
+    expected = "607eb032438128431dac0858"
+
+    assert actual == expected
+
+
+def test_get_image_stream_tag1():
+    actual = img_util.get_image_stream_tag("ose-metallb", "4.10")
     expected = None
 
     assert actual == expected
 
 
-def test_check_for_payload2():
-    actual = get_image_stream_tag("openshift-enterprise-console", "4.10")
+def test_get_image_stream_tag2():
+    actual = img_util.get_image_stream_tag("openshift-enterprise-console", "4.10")
     expected = "console"
 
     assert actual == expected
+
+
+def test_doozer_github_distgit():
+    response = img_util.doozer_github_distgit("4.10")
+
+    assert len(response) == 208
+
+
+def test_require_bundle_build1():
+    response = img_util.require_bundle_build("clusterresourceoverride-operator", "4.10")
+
+    assert response
+
+
+def test_require_bundle_build2():
+    response = img_util.require_bundle_build("openshift-enterprise-cli-alt", "4.10")
+
+    assert not response
+
+
+def test_get_bundle_override1():
+    response = img_util.get_bundle_override("special-resource-operator", "4.10")
+
+    assert response
+
+
+def test_get_bundle_override2():
+    response = img_util.get_bundle_override("clusterresourceoverride-operator", "4.10")
+
+    assert not response
