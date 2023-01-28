f30c4beabf3b254b3df1c71fe854fd17394d32a1
Ashwin Das <ashwindasr@gmail.com>
2023-01-27 22:49:02 -0500

    migrate image pipeline code to backend (#65)

--- artbotlib/pipeline_image_names.py
+++ artbotlib/pipeline_image_names.py
@@ -1,5 +1,65 @@
-from artbotlib import exceptions, constants
-from artbotlib import pipeline_image_util
+import requests
+import logging
+from artbotlib import constants
+
+logger = logging.getLogger(__name__)
+
+
+def process_data(response_data):
+    payload = ""
+    payload += f"Upstream GitHub repository: " \
+               f"<{response_data.get('upstream_github_url')}|*openshift/{response_data['github_repo']}*>\n"
+    payload += f"Private GitHub repository: " \
+               f"<{response_data.get('private_github_url')}|*openshift-priv/{response_data['github_repo']}*>\n"
+
+    distgits = response_data['distgit']
+
+    for distgit in distgits:
+        payload += f"Production dist-git repo: " \
+                   f"<{distgit['distgit_url']}|*{distgit['distgit_repo_name']}*>\n"
+        payload += f"Production brew builds: " \
+                   f"<{distgit['brew']['brew_build_url']}|" \
+                   f"*{distgit['brew']['brew_package_name']}*>\n"
+
+        if distgit['brew']['bundle_component'] != "None":
+            payload += f"Bundle Component: *{distgit['brew']['bundle_component']}*\n"
+
+        if distgit['brew']['bundle_distgit'] != "None":
+            payload += f"Bundle Component: *{distgit['brew']['bundle_distgit']}*\n"
+
+        cdns = distgit['brew']['cdn']
+        if len(cdns) > 1:
+            payload += "\n *Found more than one Brew to CDN mappings:*\n\n"
+        for cdn in cdns:
+            payload += f"CDN repo: <{cdn['cdn_repo_url']}|" \
+                       f"*{cdn['cdn_repo_name']}*>\n"
+
+            payload += f"Delivery (Comet) repo: " \
+                       f"<{cdn['delivery']['delivery_repo_url']}|" \
+                       f"*{cdn['delivery']['delivery_repo_name']}*>\n\n"
+    return payload
+
+
+def handle_request(so, version, content_name, image_type):
+    so.say("Fetching data, please wait...")
+    if not version:
+        version = "4.10"
+    url = f"{constants.ART_DASH_API_ROUTE}/" \
+          f"pipeline-image?starting_from={image_type}&name={content_name}&version={version}"
+    logger.debug("URL to server: ", url)
+    print(url)
+    response = requests.get(url)
+
+    try:
+        if response.status_code == 200 and response.json().get("status") == "success":
+            payload = process_data(response.json().get("payload"))
+            so.say(payload)
+        else:
+            raise Exception(response.json().get("payload"))
+    except Exception as e:
+        logger.error(e)
+        so.say("Error. Contact ART Team")
+        so.monitoring_say(f"Error: {e}")
 
 
 # Driver functions
@@ -19,51 +79,7 @@ def pipeline_from_github(so, github_repo, version):
                                                             https://github.com/openshift/ironic-image
     :version: OCP version
     """
-    if not version:
-        version = "4.10"  # Default version set to 4.10, if unspecified
-    variant = f"8Base-RHOSE-{version}"
-
-    payload = ""
-
-    if not pipeline_image_util.github_repo_is_available(github_repo):  # Check if the given GitHub repo actually exists
-        # If incorrect GitHub name provided, no need to proceed.
-        payload += f"No GitHub repo with name *{github_repo}* exists. Try again.\n"
-        payload += "Example format: *what is the image pipeline for github `ironic-image`*"
-        so.say(payload)
-        return
-    else:
-        so.say("Fetching data. Please wait...")
-
-        # GitHub
-        payload += f"Upstream GitHub repository: <https://github.com/openshift/{github_repo}|*openshift/{github_repo}*>\n"
-        payload += f"Private GitHub repository: <https://github.com/openshift-priv/{github_repo}|*openshift-priv/{github_repo}*>\n"
-        try:
-            # GitHub -> Distgit
-            distgit_repos = pipeline_image_util.github_to_distgit(github_repo, version)
-            if len(distgit_repos) > 1:
-                payload += f"\n*More than one dist-gits were found for the GitHub repo `{github_repo}`*\n\n"
-            for distgit_repo_name in distgit_repos:
-                payload += f"Production dist-git repo: <{constants.CGIT_URL}/containers/{distgit_repo_name}|*{distgit_repo_name}*>\n"
-
-                # Distgit -> Delivery
-                payload += pipeline_image_util.distgit_to_delivery(distgit_repo_name, version, variant)
-
-                payload += "\n"
-        except exceptions.ArtBotExceptions as e:
-            payload += "\n"
-            payload += f"{e}"
-            so.say(payload)
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except exceptions.InternalServicesExceptions as e:
-            so.say(f"{e}. Contact the ART Team")
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except Exception as e:
-            so.say("Unknown error. Contact the ART team.")
-            so.monitoring_say(f"ERROR: Unclassified: {e}")
-            return
-    so.say(payload)
+    handle_request(so, version, content_name=github_repo, image_type="github")
 
 
 def pipeline_from_distgit(so, distgit_repo_name, version):
@@ -76,47 +92,7 @@ def pipeline_from_distgit(so, distgit_repo_name, version):
     :distgit_repo_name: Name of the distgit repo we get as input
     :version: OCP version
     """
-    if not version:
-        version = "4.10"  # Default version set to 4.10, if unspecified
-    variant = f"8Base-RHOSE-{version}"
-
-    payload = ""
-
-    if not pipeline_image_util.distgit_is_available(
-            distgit_repo_name):  # Check if the given distgit repo actually exists
-        # If incorrect distgit name provided, no need to proceed.
-        payload += f"No distgit repo with name *{distgit_repo_name}* exists. Try again\n"
-        payload += "Example format: *what is the image pipeline for distgit `ironic`*"
-
-        so.say(payload)
-        return
-    else:
-        so.say("Fetching data. Please wait...")
-        try:
-            # Distgit -> GitHub
-            github_repo = pipeline_image_util.distgit_to_github(distgit_repo_name, version)
-            payload += f"Upstream GitHub repository: <https://github.com/openshift/{github_repo}|*openshift/{github_repo}*>\n"
-            payload += f"Private GitHub repository: <https://github.com/openshift-priv/{github_repo}|*openshift-priv/{github_repo}*>\n"
-
-            # Distgit
-            payload += f"Production dist-git repo: <{constants.CGIT_URL}/containers/{distgit_repo_name}|*{distgit_repo_name}*>\n"
-
-            # Distgit -> Delivery
-            payload += pipeline_image_util.distgit_to_delivery(distgit_repo_name, version, variant)
-        except exceptions.ArtBotExceptions as e:
-            payload += "\n"
-            payload += f"{e}"
-            so.say(payload)
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except exceptions.InternalServicesExceptions as e:
-            so.say(f"{e}. Contact the ART Team")
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except Exception as e:
-            so.say("Unknown error. Contact the ART team.")
-            so.monitoring_say(f"ERROR: Unclassified: {e}")
-    so.say(payload)
+    handle_request(so, version, content_name=distgit_repo_name, image_type="distgit")
 
 
 def pipeline_from_brew(so, brew_name, version):
@@ -129,46 +105,7 @@ def pipeline_from_brew(so, brew_name, version):
     :brew_name: Name of the brew repo we get as input
     :version: OCP version
     """
-    if not version:
-        version = "4.10"  # Default version set to 4.10, if unspecified
-    variant = f"8Base-RHOSE-{version}"
-
-    payload = ""
-
-    if not pipeline_image_util.brew_is_available(brew_name):  # Check if the given brew repo actually exists
-        # If incorrect brew name provided, no need to proceed.
-        payload += f"No brew package with name *{brew_name}* exists. Try again\n"
-        payload += "Example format: *what is the image pipeline for package `ironic-container`*"
-
-        so.say(payload)
-        return
-    else:
-        so.say("Fetching data. Please wait...")
-        try:
-            # Brew -> GitHub
-            payload += pipeline_image_util.brew_to_github(brew_name, version)
-
-            # Brew
-            brew_id = pipeline_image_util.get_brew_id(brew_name)
-            payload += f"Production brew builds: <{constants.BREW_URL}/packageinfo?packageID={brew_id}|*{brew_name}*>\n"
-
-            # Brew -> Delivery
-            payload += pipeline_image_util.brew_to_delivery(brew_name, variant)
-
-        except exceptions.ArtBotExceptions as e:
-            payload += "\n"
-            payload += f"{e}"
-            so.say(payload)
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except exceptions.InternalServicesExceptions as e:
-            so.say(f"{e}. Contact the ART Team")
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except Exception as e:
-            so.say("Unknown error. Contact the ART team.")
-            so.monitoring_say(f"ERROR: Unclassified: {e}")
-    so.say(payload)
+    handle_request(so, version, content_name=brew_name, image_type="brew")
 
 
 def pipeline_from_cdn(so, cdn_repo_name, version):
@@ -181,44 +118,7 @@ def pipeline_from_cdn(so, cdn_repo_name, version):
     :cdn_repo_name: Name of the CDN repo we get as input
     :version: OCP version
     """
-    if not version:
-        version = "4.10"  # Default version set to 4.10, if unspecified
-    variant = f"8Base-RHOSE-{version}"
-
-    payload = ""
-
-    if not pipeline_image_util.cdn_is_available(cdn_repo_name):  # Check if the given brew repo actually exists
-        # If incorrect brew name provided, no need to proceed.
-        payload += f"No CDN repo with name *{cdn_repo_name}* exists. Try again\n"
-        payload += "Example format: *what is the image pipeline for cdn `redhat-openshift4-ose-ironic-rhel8`*"
-
-        so.say(payload)
-        return
-    else:
-        so.say("Fetching data. Please wait...")
-        try:
-            # CDN -> GitHub
-            payload += pipeline_image_util.cdn_to_github(cdn_repo_name, version)
-
-            # CDN
-            payload += pipeline_image_util.get_cdn_payload(cdn_repo_name, variant)
-
-            # CDN -> Delivery
-            payload += pipeline_image_util.cdn_to_delivery_payload(cdn_repo_name)
-        except exceptions.ArtBotExceptions as e:
-            payload += "\n"
-            payload += f"{e}"
-            so.say(payload)
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except exceptions.InternalServicesExceptions as e:
-            so.say(f"{e}. Contact the ART Team")
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except Exception as e:
-            so.say("Unknown error. Contact the ART team.")
-            so.monitoring_say(f"ERROR: Unclassified: {e}")
-    so.say(payload)
+    handle_request(so, version, content_name=cdn_repo_name, image_type="cdn")
 
 
 def pipeline_from_delivery(so, delivery_repo_name, version):
@@ -234,51 +134,4 @@ def pipeline_from_delivery(so, delivery_repo_name, version):
                                                     ose-ironic-rhel8
     :version: OCP version
     """
-    if not version:
-        version = "4.10"  # Default version set to 4.10, if unspecified
-    variant = f"8Base-RHOSE-{version}"
-
-    payload = ""
-    delivery_repo_name = f"openshift4/{delivery_repo_name}"
-
-    if not pipeline_image_util.delivery_repo_is_available(delivery_repo_name):  # Check if the given delivery repo actually exists
-        # If incorrect delivery repo name provided, no need to proceed.
-        payload += f"No delivery repo with name *{delivery_repo_name}* exists. Try again\n"
-        payload += "Example format: *what is the image pipeline for image `openshift4/ose-ironic-rhel8`*"
-
-        so.say(payload)
-        return
-    else:
-        so.say("Fetching data. Please wait...")
-        try:
-            # Brew
-            brew_name = pipeline_image_util.brew_from_delivery(delivery_repo_name)
-            brew_id = pipeline_image_util.get_brew_id(brew_name)
-
-            # Brew -> GitHub
-            payload += pipeline_image_util.brew_to_github(brew_name, version)
-
-            # To make the output consistent
-            payload += f"Production brew builds: <{constants.BREW_URL}/packageinfo?packageID={brew_id}|*{brew_name}*>\n"
-
-            # Brew -> CDN
-            cdn_repo_name = pipeline_image_util.brew_to_cdn_delivery(brew_name, variant, delivery_repo_name)
-            payload += pipeline_image_util.get_cdn_payload(cdn_repo_name, variant)
-
-            # Delivery
-            delivery_repo_id = pipeline_image_util.get_delivery_repo_id(delivery_repo_name)
-            payload += f"Delivery (Comet) repo: <{constants.COMET_URL}/{delivery_repo_id}|*{delivery_repo_name}*>\n\n"
-        except exceptions.ArtBotExceptions as e:
-            payload += "\n"
-            payload += f"{e}"
-            so.say(payload)
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except exceptions.InternalServicesExceptions as e:
-            so.say(f"{e}. Contact the ART Team")
-            so.monitoring_say(f"ERROR: {e}")
-            return
-        except Exception as e:
-            so.say("Unknown error. Contact the ART team.")
-            so.monitoring_say(f"ERROR: Unclassified: {e}")
-    so.say(payload)
+    handle_request(so, version, content_name=delivery_repo_name, image_type="delivery")
