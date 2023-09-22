d21eaa4d876262270139f9fe7d9f8338e6b70a28
AOS Automation Release Team <noreply@redhat.com>
2023-09-22 16:22:22 +0200

    Also notify about restart for watch build feature

--- artbotlib/buildinfo.py
+++ artbotlib/buildinfo.py
@@ -8,7 +8,7 @@ from enum import Enum
 
 import koji
 
-from . import util, constants, exectools
+from . import util, constants, exectools, variables
 from .rhcos import rhcos_build_urls
 
 logger = logging.getLogger(__name__)
@@ -198,6 +198,7 @@ def brew_build_url(nvr):
 def alert_on_build_complete(so, user_id, build_id):
     so.say(f'Ok <@{user_id}>, I\'ll respond here when the build completes')
     start = time.time()
+    variables.active_slack_objects.add(so)
 
     try:
         # Has the build passed in by ID?
@@ -206,45 +207,45 @@ def alert_on_build_complete(so, user_id, build_id):
         # No, by URL
         build_id = int(build_id.split('=')[-1])
 
-    while True:
-        # Timeout after 12 hrs
-        if time.time() - start > constants.TWELVE_HOURS:
-            so.say(f'Build {build_id} did not complete in 12 hours, giving up...')
-            return
+    try:
+        while True:
+            # Timeout after 12 hrs
+            if time.time() - start > constants.TWELVE_HOURS:
+                so.say(f'Build {build_id} did not complete in 12 hours, giving up...')
+                break
 
-        # Retrieve build info
-        try:
+            # Retrieve build info
             build = util.koji_client_session().getBuild(build_id, strict=True)
             state = BuildState(build['state'])
             logger.info(f'Build {build_id} has state {state.name}')
 
-        except ValueError:
-            # Failed to convert the build state to a valid BuildState enum
-            logger.warning(f'Unexpected status {build.state} for build {build_id}')
-            so.say(f'Build {build_id} has unhandled status {state.name}. '
-                   f'Check {constants.BREW_URL}/buildinfo?buildID={build_id} for details')
-            return
+            # Check build state
+            if state == BuildState.BUILDING:
+                time.sleep(constants.FIVE_MINUTES)
 
-        except koji.GenericError:
-            # No such build
-            logger.error('No such build %s', build_id)
-            message = f"Build {build_id} does not exist"
-            so.say(message)
-            return
+            else:
+                # state in [BuildState.COMPLETE, BuildState.FAILED, BuildState.CANCELED]:
+                so.say(f'Build {build_id} completed with status {state.name}. '
+                       f'Check {constants.BREW_URL}/buildinfo?buildID={build_id} for details')
+                break
 
-        except Exception as e:
-            # What else can happen?
-            message = f"error getting information for build {build_id}: {e}"
-            logger.error(message)
-            so.say(message)
-            return
+    except ValueError:
+        # Failed to convert the build state to a valid BuildState enum
+        logger.warning(f'Unexpected status for build {build_id}')
+        so.say(f'Unexpected status for build {build_id}. '
+               f'Check {constants.BREW_URL}/buildinfo?buildID={build_id} for details')
 
-        # Check build state
-        if state == BuildState.BUILDING:
-            time.sleep(constants.FIVE_MINUTES)
+    except koji.GenericError:
+        # No such build
+        logger.error('No such build %s', build_id)
+        message = f"Build {build_id} does not exist"
+        so.say(message)
 
-        else:
-            # state in [BuildState.COMPLETE, BuildState.FAILED, BuildState.CANCELED]:
-            so.say(f'Build {build_id} completed with status {state.name}. '
-                   f'Check {constants.BREW_URL}/buildinfo?buildID={build_id} for details')
-            return
+    except Exception as e:
+        # What else can happen?
+        message = f"error getting information for build {build_id}: {e}"
+        logger.error(message)
+        so.say(message)
+
+    finally:
+        variables.active_slack_objects.remove(so)
